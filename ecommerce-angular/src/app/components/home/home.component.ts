f6f33f8e16127d2ac86bdb85854c0d0e66a6bc3a
lima73777@gmail.com <lima73777@gmail.com>
2024-06-17 18:49:40 -0300

    slider multiple items para productos y logos, para logos listo, pero para productos destacados falta pulir y elegir

--- ecommerce-angular/src/app/components/home/home.component.ts
+++ ecommerce-angular/src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { CategoryService } from '../../services/category.service';
 import { UserService } from '../../services/user.service';
@@ -10,6 +10,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { environment } from '../../../environments/environment.prod';
 import { Imagen } from '../../models/imagen';
 import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
@@ -23,8 +24,21 @@ export class HomeComponent implements OnInit{
   baseUrl = environment.baseUrl;
   productsHighlights: ProductRequest[] = [];
   image:Imagen;
-
+  @ViewChild('carousel') carousel!: ElementRef;
   
+  cards = [
+    { title: 'Card title 1', text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.' },
+    { title: 'Card title 2', text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.' },
+    { title: 'Card title 3', text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.' },
+    { title: 'Card title 4', text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.' },
+    { title: 'Card title 5', text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.' },
+
+    // ... agrega más tarjetas aquí
+  ];
+
+  carouselWidth!: number;
+  cardWidth!: number;
+  scrollPosition = 0;
 
   constructor(private productService:ProductService,private categoryService:CategoryService,private userService:UserService, private formBuilder:FormBuilder, private loginService:LoginService,private router:Router ){
     this.image = new Imagen();
@@ -32,8 +46,37 @@ export class HomeComponent implements OnInit{
 
   ngOnInit(): void {
     this.getAllProductbyHighlight();
+   
+  }
+  ngAfterViewInit(): void {
+    this.initCarousel();
+  }
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.initCarousel();
   }
 
+  initCarousel(): void {
+    if (window.matchMedia('(min-width: 768px)').matches) {
+      const carouselInner = this.carousel.nativeElement.querySelector('.carousel-inner-new');
+      this.carouselWidth = carouselInner.scrollWidth;
+      this.cardWidth = carouselInner.querySelector('.carousel-item').offsetWidth;
+    }
+  }
+
+  prevSlide(): void {
+    if (this.scrollPosition > 0) {
+      this.scrollPosition -= this.cardWidth;
+      this.carousel.nativeElement.querySelector('.carousel-inner-new').scrollLeft = this.scrollPosition;
+    }
+  }
+
+  nextSlide(): void {
+    if (this.scrollPosition < (this.carouselWidth - (this.cardWidth * 4))) {
+      this.scrollPosition += this.cardWidth;
+      this.carousel.nativeElement.querySelector('.carousel-inner-new').scrollLeft = this.scrollPosition;
+    }
+  }
   getAllProductbyHighlight(): void {
     this.productService.getAllProductsHighlight().subscribe(
       (data: ProductRequest[]) => {
